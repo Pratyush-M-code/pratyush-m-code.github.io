55bae31cea661f78f0eaa9e0f5fdb37145d6cff8
Pratyush <pratyush.m110@gmail.com>
2026-09-20 14:20:25 +0530

    Text typing animation and Caret blinking
    Added the typing animation for the header of my quote and added a new caret animation. I used both javascript and CSS! It's quite fun learning how to use KeyFrames!

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Happy developing ✨')
+const pause = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function type(id,randomness){
+    let element = document.getElementById(id)
+    let text = element.textContent
+    let char = [...text]
+    let quote = ""
+    let current = []
+    for (let i = 0; i < text.length; i++){
+        let target = char[i]
+        current.push(target)
+        quote = current.join("")
+        element.textContent = quote
+        console.log(quote)
+        if(target != " "){
+            await pause(150);
+        }
+        else{
+            await pause(300);
+        }
+    }
+}
+
+type("home-quote",1)
